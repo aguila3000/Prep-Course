14d2b5d30c5874f1e0fbba9708a3aa20026b5c40
Luis <aguila3000@gmail.com>
2022-06-11 22:08:42 -0500

    Segundo prueba usando ATOM

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 256;
+const nuevoNum = 126;
 
 // Crea una variable booleana:
 const nuevoBool = null;
